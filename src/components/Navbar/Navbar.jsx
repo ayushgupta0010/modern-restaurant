12b870954c315256da09fc85e4276313a81201fe
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-03-15 21:10:54 +0530

    Add components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const closeToggle = () => {
+    setToggleMenu(false);
+  };
+
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt='app logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'>
+          <a href='#home'>Home</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#about'>About</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#menu'>Menu</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#awards'>Awards</a>
+        </li>
+        <li className='p__opensans'>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+      <div className='app__navbar-login'>
+        <a href='#login' className='p__opensans'>
+          Log In / Registration
+        </a>
+        <div />
+        <a href='/' className='p__opensans'>
+          Book Table
+        </a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu
+          color='#fff'
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className='overlay__close'
+              onClick={closeToggle}
+            />
+            <ul className='app__navbar-smallscreen_links'>
+              <li>
+                <a href='#home' onClick={closeToggle}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#about' onClick={closeToggle}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href='#menu' onClick={closeToggle}>
+                  Menu
+                </a>
+              </li>
+              <li>
+                <a href='#awards' onClick={closeToggle}>
+                  Awards
+                </a>
+              </li>
+              <li>
+                <a href='#contact' onClick={closeToggle}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
